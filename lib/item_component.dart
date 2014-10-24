2003e7c909864f451aa4f74f8d7ed0b277e66d91
Joel Trottier-Hebert <iamquebecois@gmail.com>
2014-10-23 23:27:48 -0400

    Striped text

--- lib/item_component.dart
+++ lib/item_component.dart
@@ -2,11 +2,21 @@ library item_component;
 
 import 'package:angular/angular.dart';
 import 'package:grocery_store/item.dart';
+import 'dart:html';
 
 @Component(
     selector: 'item',
-    templateUrl: 'packages/grocery_store/item_component.html')
+    templateUrl: 'packages/grocery_store/item_component.html',
+    cssUrl: 'packages/grocery_store/item.css'
+)
 class ListItemComponent {
     @NgTwoWay('item')
     Item item;
+
+    @NgTwoWay('selected')
+    bool selected;
+
+    void onSelect() {
+
+    }
 }
